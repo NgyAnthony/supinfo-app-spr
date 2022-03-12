3a526dcde3122e713fb1d5ffb00cac275bff37bd
Anthony NGUYEN <anthony.nguyen@enedis.fr>
2022-03-12 11:03:39 +0100

    update 12/03

--- pages/index.js
+++ pages/index.js
@@ -116,7 +116,7 @@ export default function Home({ user, points }) {
                     <Divider mb={4} mt={4}/>
                     <Text fontSize='md'>XLIF est une matière vous rapportant 3 crédits ECTS. Vous validez la matière en ayant au minimum 10 points sur 20. Chaque évènement (Journée Portes Ouvertes, salon étudiant, accompagnement étudiant, visite lycée...) vous rapporte un nombre de points.</Text>
                     <Divider mb={4} mt={4}/>
-                    <Text fontSize='xl'>Voici votre relevé de points XLIF (dernière MAJ le 12 Février)</Text>
+                    <Text fontSize='xl'>Voici votre relevé de points XLIF (dernière MAJ le 12 Mars)</Text>
                     {
                         getValidatedOrNot(sumOfPoints)
                     }
